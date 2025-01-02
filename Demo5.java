4e30e62033184d53a3e7120d4b1720394928c71d
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-02 15:19:09 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    public static void main(String[] args) {
+    String username = "user001";
+    String pwd = "user_001";
+    while((username == "user001") && (pwd == "user001")) {
+        System.out.println("Happy new year");
+        break;
+    }
+ }
+}
+
+
