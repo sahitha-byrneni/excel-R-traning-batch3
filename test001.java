88be888a3dcd11006c8877c6bcc450ac8e4f3fa2
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-11 09:27:34 +0530

    Add files via upload

---
+++ test001.java
@@ -0,0 +1,13 @@
+public class test001{
+    public static void main(String[] args){
+        int i;
+        for (i = 1; i <= 10; i++){
+            if(i == 5 || i == 7){
+
+
+             continue;
+            }
+             System.out.println(i); 
+            }
+        }
+    }
