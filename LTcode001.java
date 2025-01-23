2c359fe6271996eb29b753f6d94314b75ce190c8
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-23 11:08:09 +0530

    palindrome code using leet code

---
+++ LTcode001.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Solution {
+    public boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        if(number<0){
+            return false;
+        }
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class LTcode001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        Solution sol = new Solution();
+        boolean result = sol.isPalindrome(number);
+        System.out.println(result);
+        sc.close();
+    }
+}
