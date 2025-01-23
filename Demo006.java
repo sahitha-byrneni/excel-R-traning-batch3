58b08e721bdfb12678d694df95aeef55a0c6b786
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-23 12:29:43 +0530

    username and password

---
+++ Demo006.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Demo006{
+    public static void main(String[] args) {
+        boolean a = true;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter username: ");
+            String id = sc.next();
+            System.out.print("Enter password: ");
+            String pass = sc.next();
+            if((id.equals("sahitha")) && (pass.equals("sahitha_123"))){
+                System.out.println("Hello sahitha, You are welcome ");
+                break;
+            }else{
+                System.out.println("credentials does not match. Please try again.");
+            }
+        }
+    }
+}
+
