20a2c37898ff85510e4ae697fa55654fd8636b25
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-23 18:54:55 +0530

    1-10 expect 5 and 7

---
+++ Test001.java
@@ -0,0 +1,11 @@
+public class Test001 {
+    public static void main (String[] args) {
+        int i;
+        for(i = 1; i<= 10; i++){
+        if(i == 5 || i == 7){
+            continue;
+        }
+        System.out.println(i);
+    }
+}
+}
