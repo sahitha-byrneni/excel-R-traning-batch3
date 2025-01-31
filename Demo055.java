e00924958c51ca2474bf2877d2f88a53474b124d
sahitha-byrneni <sahitharao51@gmail.com>
2025-01-31 12:41:12 +0530

    To print sum of squares of a digit of a number

---
+++ Demo055.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Demo055 {
+    static int sumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println("Sum of squares of digits: " + sumOfSquares(num));
+        scanner.close();
+    }
+}
