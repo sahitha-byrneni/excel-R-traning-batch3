f0303bf210b2d8b3c68451107ff955561cd25145
sahitha-byrneni <sahitharao51@gmail.com>
2025-02-06 12:04:23 +0530

    print array elements

---
+++ Demo065.java
@@ -0,0 +1,7 @@
+
+import java.util.Arrays;
+class Demo065 {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5};
+       System.out.println("Array elements: " + Arrays.toString(arr));
+    }}
